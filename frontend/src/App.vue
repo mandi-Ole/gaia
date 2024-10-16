55709d4c3199c59794b0ab15b0eccb302e0b1cd4
mandi ole <mandi@pixelschrauber.at>
2024-10-16 12:52:20 +0200

    Initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Hallo Welt!1'
+    };
+  }
+};
+</script>
+
+<style>
+/* Optionales CSS */
+</style>
+
+  
